29fac34f421568453dbc653b7dc889fc545133a8
edriball121 <53952759+edriball121@users.noreply.github.com>
2023-01-17 19:36:11 -0500

    agregando ejercicio final
    agregando ejercicio final

--- python/tuplas_Diccionarios.py
+++ python/tuplas_Diccionarios.py
@@ -301,3 +301,20 @@ polEspDict = {
 
 copyDict = polEspDict.copy()
 #------------------------------------------
+
+#------------------------------------------
+#ejercicio final
+#------------------------------------------
+
+#Tu tarea es escribir un simple programa que simule jugar a tic-tac-toe (nombre en inglés) con el usuario. 
+# Para hacerlo más fácil, Hemos decidido simplificar el juego. Aquí están nuestras reglas:
+#La maquina (por ejemplo, el programa) jugará utilizando las 'X's.
+#El usuario (por ejemplo, tu) jugará utilizando las 'O's.
+#El primer movimiento es de la maquina: siempre coloca una 'X' en el centro del tablero.
+#Todos los cuadros están numerados comenzando con el 1 (observa el ejemplo para que tengas una referencia).
+#El usuario ingresa su movimiento introduciendo el numero de cuadro elegido. El numero debe de ser valido, 
+# por ejemplo un valor entero mayor que 0 y menor que 10, y no puede ser un cuadro que ya esté ocupado.
+#El programa verifica si el juego ha terminado. Existen cuatro posibles veredictos: el juego continua, el juego termina en empate, tu ganas, o la maquina gana.
+#La maquina responde con su movimiento y se verifica el estado del juego.
+#No se debe implementar algún tipo de inteligencia artificial, la maquina elegirá un cuadro de manera aleatoria, eso es suficiente para este juego.
+#El ejemplo del programa es el siguiente:
